a428a1cf773331b02fb3ae89d7c42ca766afbb8e
Matojeje <matojeje@gmail.com>
2018-08-24 21:23:10 +0200

    Update version

--- commands/about.js
+++ commands/about.js
@@ -17,10 +17,10 @@ module.exports = {
 		const riiInfo = new Discord.RichEmbed()
 			.setColor("#2990bb")
 			.setAuthor("About mato-bot", "https://i.imgur.com/Hny5LS4.png", "")
-			.setTitle("v1.0.5 (indev)")
-			.setDescription("5. 8. 2018\nRunning on Discord.js\n\nUse " + prefix + "help to check the available commands.")
+			.setTitle("v1.1.0-H (indev)")
+			.setDescription("24. 8. 2018\nRunning on Heroku using Discord.js!\n\nUse " + prefix + "help to check the available commands.")
 			.addField("Uptime", client.uptime + " milliseconds, since " + client.readyAt, true)
-			.addField("Ping", client.ping + " milliseconds", true)
+			.addField("Ping", math.round(client.ping) + " milliseconds", true)
 			.setImage('https://i.imgur.com/akVZfty.png')
 			.setTimestamp()
 			.setFooter('By Mato', "https://cdn.discordapp.com/avatars/189400498497912832/d7dc389f5d85cccde8468e68911648d6.png");
