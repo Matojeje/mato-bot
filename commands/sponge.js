330f3a94f5559e883dde92f40ed4119e77dbf4c4
Matojeje <maty251@gmail.com>
2019-02-07 11:21:38 +0100

    Add ;sponge (request)

---
+++ commands/sponge.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: 'sponge',
+	errorVerb: "widen the sponge",
+	missingArgsVerb: "widening",
+	aliases: ['widen','widen-sponge'],
+	cooldown: 20,
+	guildOnly: false,
+	description: "(Request) This command widens the sponge.",
+	usage: "\n",
+
+
+	execute(message, args) {
+		message.reply("https://youtu.be/wYaLrT30I1I?t=136")
+
+	},
+};
