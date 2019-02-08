ece90937b39f5581323c192fdfbb388c32ef17eb
Matojeje <maty251@gmail.com>
2019-02-08 20:33:08 +0100

    Fixed hopefully

--- commands/riolu.js
+++ commands/riolu.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
+const Discord = require('discord.js');
 
 const GoogleImages = require("google-images");
-const { Client, Attachment } = require("discord.js");
-const client = new Client();
-const googleImages = new GoogleImages(
-	process.env.CSE_ID, // CSE ID
-	process.env.API_KEY // API Key
-);
+const googleImages = new GoogleImages(process.env.CSE_ID, process.env.API_KEY);
 
 module.exports = {
-	name: 'rioulupics',
+	name: 'riolu',
 	errorVerb: "look up Riolu pics",
 	missingArgsVerb: "Riolu",
 
@@ -20,15 +16,16 @@ module.exports = {
 	description: "This command will look up a random Riolu picture from Google Images.",
 	usage: "\n",
 
-// This function is used to get a random image from Google and then send it to Discord
-execute(message, args, client) {
-	try {
-		const results = googleImages.search("Riolu"); // Indexing Google images
-		const reply = !results
-			? "No results"
-			: new Attachment(results[Math.floor(Math.random() * results.length)].url); // Randomize output
-		message.channel.send(reply);
-	} catch (e) {
-		return e;
-	}
-}
+	// This function is used to get a random image from Google and then send it to Discord
+	execute(message, args, client) {
+		try {
+			const results = googleImages.search("Riolu"); // Indexing Google images
+			const reply = !results
+				? "No results"
+				: new Attachment(results[Math.floor(Math.random() * results.length)].url); // Randomize output
+			message.channel.send(reply);
+		} catch (e) {
+			return e;
+		}
+	},
+};
