ace79e72b2f14a34be63a996e002d3a9c7071df5
Matojeje <matojeje@gmail.com>
2019-02-25 23:03:42 +0100

    Add ;boop

---
+++ commands/boop.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "boop",
+    errorVerb: "boop someone",
+    missingArgsVerb: "booping",
+    aliases: ["bop", "bloop"],
+    cooldown: 5,
+    guildOnly: false,
+    args: true,
+    description: "This command will boop someone.",
+    shortDesc: "Boop someone",
+    usage: "[__user__]",
+
+    execute(message, args, client) {
+        message.channel.send(`*boops ${args[0]}!*`)
+    },
+};
