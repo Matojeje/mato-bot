69f582d99d736781712521784575815aeeb77812
Matojeje <maty251@gmail.com>
2020-03-14 22:41:56 +0100

    Redesign ;about command

--- source/commands/about.js
+++ source/commands/about.js
@@ -1,82 +1,74 @@
 "use strict";
 
-import Discord from "discord.js";
+import { MessageEmbed, MessageAttachment } from "discord.js";
 
 require("dotenv").config();
 
 const meta = {
-    version: "1.8",
-    timestamp: new Date(1570314700000), // Time of last edit (±) from Date.now()
+    version: "1.9",
+    timestamp: 1584223141592, // Time of last edit (roughly) from Date.now()
 };
 
 export default {
     name: "about",
     errorVerb: "tell you about myself",
     missingArgsVerb: "rii",
-    aliases: ["bot"],
+    aliases: ["bot", "info"],
     cooldown: 40,
     shortDesc: "Shows info about the bot",
     description: `In ${process.env.PREFIX}about, I tell you a little about myself!`,
     guildOnly: false,
-    usage: "**credits** to include credits",
 
     execute({ channel }, args, client) {
-        const badge = new Discord.MessageAttachment(
+        const badge = new MessageAttachment(
             "resources/badgeAbout.png",
             "badge.png",
         );
-        const drawing = new Discord.MessageAttachment(
+        const drawing = new MessageAttachment(
             "resources/drawingBotHD.png",
             "drawing.png",
         );
 
+        const cookies = Math.round(1.25 * (Math.random() * 7.7) ** 2);
+
         const blank = "\u200B";
 
-        const riiInfo = new Discord.MessageEmbed()
+        const riiInfo = new MessageEmbed()
             .setColor("#2990bb")
             .setAuthor("About mato-bot", "attachment://badge.png", "")
             .setTitle(`**v${meta.version}** (in development)`)
-            .setDescription(
-                new Date(meta.timestamp).toLocaleString() +
-					`
-Running on Discord.js!
+            .setDescription(`
+Running on Discord.js v12!
 
 Use **${process.env.PREFIX}help** to check the available commands.
-https://github.com/Matojeje/mato-bot`,
-            )
-            .addField("Uptime", `${Math.round(client.uptime)} ms`, true)
-            .addField("Woke up at", client.readyAt.toLocaleString(), true)
-            .addField("Ping", `${client.ws.ping.toFixed(1)} ms`, true)
-            .addField(
-                "Cookies in belly",
-                Math.round(Math.random() * 5000),
-                true,
-            )
-        /* .setURL("https://github.com/Matojeje/mato-bot") */
-            .setThumbnail("attachment://drawing.png")
-            .setTimestamp(new Date(meta.timestamp));
+https://github.com/Matojeje/mato-bot
 
-        if (args[0] === "credits") {
-            riiInfo
-                .addField("\u200b", "\u200b", false) // NOTE(alt): This does the same thing as .addBlankField in Discord.js v11; They removed it for some stupid reason.
-                .addField("🔵 **Credits**", "~~             ~~")
-                .addField(
-                    "Code:",
-                    `
-\\🍪 Mato | https://github.com/Matojeje
-\\🍪 Rifki K. | https://github.com/IamRifki`,
-                    false,
-                )
-                .addField(
-                    "Artwork:",
-                    `
-\\🍪 HavocDusk | https://DuskyUmbreon.deviantart.com
-\\🍪 Spray-POKA | https://Spray-POKA.deviantart.com
-\\🍪 Parapetch | https://twitter.com/Parapetch_/`,
-                    false,
-                );
-        } else if (args[0]) {
-            riiInfo.addField("He", "<a:He:608354487836475394>", true);
+Mato-bot currently has ${cookies} cookies in his belly.`,
+            )
+            .addField("Uptime",
+                client.uptime < 15 * 60 * 1000 // 15 minutes
+                    ? `${Math.round(client.uptime / 1000)} seconds`
+                    : `${Math.round(client.uptime / 60000)} minutes`,
+                true)
+            .addField("Woke up at", client.readyAt.toLocaleString(), true)
+            .addField("Ping", `${Math.round(client.ws.ping)} ms`, true)
+            .addField("~~⠀           ⠀~~", "🔵 **Credits**")
+            .addField(
+                "<:blank:688476368471982165> Code:", `
+🍪 Mato | https://github.com/Matojeje
+🍪 Dania Rifki | https://github.com/IamRifki`,
+            )
+            .addField(
+                "<:blank:688476368471982165> Artwork:", `
+🍪 HavocDusk | https://DuskyUmbreon.deviantart.com
+🍪 Spray-POKA | https://Spray-POKA.deviantart.com
+🍪 Parapetch | https://twitter.com/Parapetch_/`,
+            )
+            .addField("~~⠀           ⠀~~", blank)
+            .setImage("attachment://drawing.png")
+            .setTimestamp(new Date(meta.timestamp));
+        if (args[0]) {
+            riiInfo.addField("<a:He:608354487836475394>", blank, true);
         }
 
         client.users
