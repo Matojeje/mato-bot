8c66f3cfa4aa4455b534d6717d0999a9e45a9638
Matojeje <maty251@gmail.com>
2020-04-10 04:15:33 +0200

    Fixed nickname handling, included bots in ;someone

--- source/commands/someone.js
+++ source/commands/someone.js
@@ -12,38 +12,39 @@ export default {
         "randusr",
         "ru",
         "who",
+        "pick",
     ],
     cooldown: 4,
     guildOnly: true,
     args: false,
     shortDesc: "Randomly picks one server member",
     description:
-		"This command will return the name of a randomly picked user on this text channel. \
-		 It will not tag them. Doesn't work in DMs. Doesn't return bots.",
-    usage: "",
+		"This command will return the name of a randomly picked user in this text channel." +
+        "It will not tag them. Doesn't work in DMs.",
+    usage: "(**bots**) to enable picking bots",
 
-    execute({ guild, channel }) {
+    execute({ guild, channel }, args, client) {
         if (guild.available) {
             let randomMember;
-            let member;
+            let picked = false;
+            let isBot = false;
+            const wantBots = args[0] === "bots";
 
-            while (true) {
+            while (!picked) {
                 randomMember = channel.members.random();
-                if (!randomMember.user.bot) {
-                    break;
-                } else {
-                    continue;
-                }
+                isBot = randomMember.user.bot;
+                console.log("Trying out", randomMember.user.username, { isBot, wantBots });
+                if (!isBot || wantBots) { picked = true; }
             }
 
+            const botBadge = isBot ? "`[bot]`" : "";
+            const nickname = randomMember.nickname ? `(***${randomMember.nickname + botBadge}***) ` : "";
             // NOTE(alt): As of Discord.js ~12.0, users with no username gets read as undefined instead of null.
-            if (randomMember.nickname !== undefined) {
-                member = `***${randomMember.user.username}*** (**${randomMember.nickname}**)`;
-            } else {
-                member = `**${randomMember.user.username}**`;
-            }
-
-            channel.send(`${member} was randomly picked!`);
+            channel.send(
+                randomMember.id === client.user.id
+                    ? "**Woah!** I randomly picked myself!!"
+                    : `**${ randomMember.user.username + botBadge }** ${ nickname }was randomly picked!`,
+            );
         }
     },
 };
