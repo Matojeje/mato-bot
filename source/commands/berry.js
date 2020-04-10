98f89ed5311d8479c43a7006b6f8f14bee3cd8c3
Matojeje <maty251@gmail.com>
2020-04-10 03:07:00 +0200

    Fixed and updated the ;berry command (global vars)

--- source/commands/berry.js
+++ source/commands/berry.js
@@ -14,24 +14,31 @@ export default {
     description:
 		"Gets information about a random/specific pokémon berry or a list of them (using Pokéapi).",
     shortDesc: "Look up berry info",
-    usage: "(**list**/[*berry name*])",
+    usage:
+        "(**list** / [*berry name*]) (**sort**)\n" +
+        "\nOptional | Arg 1 **`list`**: Display a list of all the available Berries" +
+        "\nOptional | Arg 1: Specify the Berry you want to get information about, or leave blank for a random Berry." +
+        "\nConditional | Arg 2 **`sort`**: Sort the Berry list alphabetically",
 
     execute({ channel }, args) {
         P.getBerriesList()
             .then(({ results, count }) => {
                 if (args[0] && args[0].toLowerCase() === "list") {
-                    channel.send("**Current list of berries**:");
-                    berries = [];
+                    const berries = [];
                     results.forEach(({ name }) => {
-                        berries.push(name);
+                        berries.push(capitalizeFirstLetter(name));
                     });
-                    channel.send(berries.join(", "));
+                    channel.send(`**Current list of berries**:\n*${(
+                        (args[1] && args[1].toLowerCase() === "sort")
+                            ? berries.sort()
+                            : berries
+                    ).join(", ")}*`);
                 } else {
-                    berryName = args[0]
-                        ? args[0].toLowerCase().match(/^[a-z]+/gm)[0]
+                    let berryName = args[0]
+                        ? args[0].toLowerCase().match(/^[a-z]+/gm)[0] // Get the first word in lower case
                         : results[getRandomInt(1, count)].name;
 
-                    console.log(`berry name:${berryName}`);
+                    console.log(`berry name: ${berryName}`);
 
                     P.getItemByName(`${berryName}-berry`)
                         .then(berryItem => {
@@ -40,10 +47,8 @@ export default {
                                     const capitalizedBerryName = capitalizeFirstLetter(
                                         berryName,
                                     );
-                                    console.log(
-                                        `berry: ${capitalizedBerryName}`,
-                                    );
 
+                                    let berryInfoText = "";
                                     const flavorTexts =
 										berryItem.flavor_text_entries;
                                     for (const x in flavorTexts) {
@@ -53,7 +58,7 @@ export default {
 											flavorTexts[x].version_group.name ==
 												"ultra-sun-ultra-moon"
                                         ) {
-                                            infoText = flavorTexts[x].text;
+                                            berryInfoText = flavorTexts[x].text;
                                             break;
                                         }
                                     }
@@ -67,8 +72,8 @@ export default {
                                     }
 
                                     const flavors = berry.flavors;
-                                    berryFlavor = "";
-                                    numberOfFlavors = 0;
+                                    let berryFlavor = "";
+                                    let numberOfFlavors = 0;
                                     for (const x in flavors) {
                                         // console.log(flavors[x].flavor.name + ": " + flavors[x].potency)
                                         if (flavors[x].potency > 0) {
@@ -84,27 +89,27 @@ export default {
                                         }
                                     }
 
-                                    berrySizeMetric =
+                                    const berrySizeMetric =
 										berry.size >= 100
 										    ? `${berry.size / 10} cm`
-										    : (berrySizeMetric = `${berry.size} mm`);
+										    : `${berry.size} mm`;
 
                                     const image = new Discord.MessageAttachment(
                                         berryItem.sprites.default,
-                                        "image.png",
+                                        `${capitalizedBerryName}Berry.png`,
                                     );
-                                    const sprite = new Discord.MessageAttachment(
+                                    /* const sprite = new Discord.MessageAttachment(
                                         berryItem.sprites.default,
                                         `${capitalizedBerryName} berry.png`,
-                                    );
-
-                                    const mmToInch = 25.4 ** -1;
+                                    ); */
 
                                     const moveInfo = buildBerryEmbed(
                                         capitalizedBerryName,
                                         berryItem,
                                         berry,
-                                        mmToInch,
+                                        berryFlavor,
+                                        berrySizeMetric,
+                                        berryInfoText,
                                     );
 
                                     const badge = new Discord.MessageAttachment(
@@ -116,43 +121,47 @@ export default {
                                         "icon.png",
                                     );
 
-                                    channel.send({
+                                    channel.send(args[0] ? null : `I picked the ${capitalizedBerryName} berry!`, {
                                         embed: moveInfo,
-                                        files: [sprite, badge, image, icon],
+                                        files: [badge, image, icon],
                                     });
                                 })
-                                .catch(error => errorPokeAPI(error));
+                                .catch(error => errorPokeAPI(error, channel));
                         })
-                        .catch(error => errorPokeAPI(error));
+                        .catch(error => errorPokeAPI(error, channel));
                 }
             })
-            .catch(error => errorPokeAPI(error));
+            .catch(error => errorPokeAPI(error, channel));
     },
 };
 
 function buildBerryEmbed(
     capitalizedBerryName,
-    { effect_entries },
+    { effect_entries, cost },
     berry,
-    mmToInch,
+    flavor,
+    sizeMetric,
+    infoText,
 ) {
+    const mmToInch = 25.4 ** -1;
     return (
         new Discord.MessageEmbed()
             .setColor("#2990bb")
             .setTitle(`**${capitalizedBerryName} Berry**`)
             .setAuthor("Berry info", "attachment://badge.png", "")
-            .setImage("attachment://image.png")
+            .setThumbnail(`attachment://${capitalizedBerryName}Berry.png`)
             .setURL(
                 `https://bulbapedia.bulbagarden.net/wiki/${capitalizedBerryName}_Berry`,
             )
             .setDescription(
+                `${infoText.replace(/\n/g, " ")} Costs ${cost || "???"} Poké.\n\n` +
                 effect_entries[0].effect
                     .replace(/\n: /gm, ": ")
                     .replace(/^.*:/gm, "**$&**"),
             )
             .addField(
                 "Flavor",
-                capitalizeFirstLetter(berryFlavor) || "???",
+                capitalizeFirstLetter(flavor) || "???",
                 true,
             )
             .addField(
@@ -175,17 +184,17 @@ function buildBerryEmbed(
             )
             .addField(
                 "Size",
-                `${berrySizeMetric} / ${parseFloat(
+                `${sizeMetric} / ${parseFloat(
                     (berry.size * mmToInch).toFixed(2),
                 )}″` || "???",
                 true,
             )
-        // .addField("Cost", berryItem.cost + " Poké" || "???", true)
-        /* .addField("Natural gift",
-        capitalizeFirstLetter(berry.natural_gift_type.name) +
-        " (power: " + berry.natural_gift_power + ")"
-        || "??? ", true
-    ) */
+            /* .addField("Cost", cost + " Poké" || "???", true)
+            .addField("Natural Gift effect",
+                capitalizeFirstLetter(berry.natural_gift_type.name) +
+                " (power: " + berry.natural_gift_power + ")"
+            || "??? ", true,
+            ) */
             .setTimestamp()
             .setFooter("Mato bot using PokéAPI", "attachment://icon.png")
     );
@@ -196,13 +205,14 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+function getRandomInt(rangeMin, rangeMax) {
+    const min = Math.ceil(rangeMin);
+    const max = Math.floor(rangeMax);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function errorPokeAPI(error) {
+function errorPokeAPI(error, channel) {
     console.log(`Pokédex API error:\n\n${error}`);
-    message.channel.send(`**PokéAPI error**:\n${error}`);
+    channel.send(`**PokéAPI error**: \`${error}\``);
+    throw new Error(error);
 }
