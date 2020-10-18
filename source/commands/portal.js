eb583a54684f11db25d4e2a4e4ffbd34b888b792
iamrifki <iamrifki0@gmail.com>
2020-10-18 19:09:49 +0700

    Slight cleanup

--- source/commands/portal.js
+++ source/commands/portal.js
@@ -7,20 +7,16 @@ export default {
     aliases: ["brazil", "yagt", "zagreb", "nether", "netherportal"],
     cooldown: 10,
     guildOnly: false,
-    args: true,
+    args: false,
     description:
         "This command will build a portal from an emoji to send people through.",
     shortDesc: "Builds a Nether portal from an emoji",
     usage: "([__emote__])",
 
     execute(message, args) {
+        const block = args[0] ? args[0] : "<:obsidian:767344800100909066>";
+        const portal = "<a:portal:767343567701016596>";
         let reply;
-        let block = "<:obsidian:767344800100909066>";
-        let portal = "<a:portal:767343567701016596>";
-
-        if (args[0]) {
-            block = args[0];
-        }
 
         if (args.length > 1) {
             reply = "```js\nError: I can only accept one argument, I am sorrii.```";
