17f08171003209172dbfa4e1af57029ed240a2b6
Matojeje <maty251@gmail.com>
2020-10-18 13:59:05 +0200

    Add custom emojis to ;portal

--- source/commands/portal.js
+++ source/commands/portal.js
@@ -4,26 +4,32 @@ export default {
     name: "portal",
     errorVerb: "build a portal",
     missingArgsVerb: "portal",
-    aliases: ["brazil", "yagt", "zagreb"],
+    aliases: ["brazil", "yagt", "zagreb", "nether", "netherportal"],
     cooldown: 10,
     guildOnly: false,
     args: true,
     description:
         "This command will build a portal from an emoji to send people through.",
-    shortDesc: "Builds a portal from an emoji",
-    usage: "[__emote__]",
+    shortDesc: "Builds a Nether portal from an emoji",
+    usage: "([__emote__])",
 
     execute(message, args) {
         let reply;
+        let block = "<:obsidian:767344800100909066>";
+        let portal = "<a:portal:767343567701016596>";
+
+        if (args[0]) {
+            block = args[0];
+        }
 
         if (args.length > 1) {
             reply = "```js\nError: I can only accept one argument, I am sorrii.```";
         } else {
-            reply = `${args[0]}${args[0]}${args[0]}${args[0]}\n`;
-            reply += `${args[0]}:purple_square::purple_square:${args[0]}\n`;
-            reply += `${args[0]}:purple_square::purple_square:${args[0]}\n`;
-            reply += `${args[0]}:purple_square::purple_square:${args[0]}\n`;
-            reply += `${args[0]}${args[0]}${args[0]}${args[0]}\n`;
+            reply = `${block}${block}${block}${block}\n`;
+            reply += `${block}${portal}${portal}${block}\n`;
+            reply += `${block}${portal}${portal}${block}\n`;
+            reply += `${block}${portal}${portal}${block}\n`;
+            reply += `${block}${block}${block}${block}\n`;
         }
 
         message.channel.send(reply);
